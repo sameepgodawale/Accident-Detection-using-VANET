fc009377b537da287a2ee701f9c92ceb5599b3b3
Sameep Godawale <57657376+sameepgodawale@users.noreply.github.com>
2025-09-25 15:16:57 +0530

    Delete vanet-system/OBU directory

--- vanet-system/OBU/obu-firmware/firmware/drivers/gps_sim.c
+++
@@ -1,3 +0,0 @@
-#include "../include/gps.h"
-#include <time.h>
-int gps_read(gps_fix_t *f){ f->lat = 17.385044; f->lon = 78.486671; f->sats=8; f->valid=1; return 0; }
--- vanet-system/OBU/obu-firmware/firmware/drivers/imu_sim.c
+++
@@ -1,7 +0,0 @@
-#include "../include/imu.h"
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-
-void imu_init(void){ srand(time(NULL)); }
-int imu_read(imu_sample_t* s){ s->ax = (rand()%200-100)/10.0f; s->ay=0; s->az=0; return 0; }
