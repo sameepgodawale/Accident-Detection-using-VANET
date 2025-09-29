b8164b1492e8f66e856fa3c5cca52de8de166ff2
Sameep Godawale <57657376+sameepgodawale@users.noreply.github.com>
2025-09-30 00:31:48 +0530

    Add files via upload

---
+++ vanet-system/RSU/firmware/src/config.h
@@ -0,0 +1,13 @@
+#pragma once
+#define CMS_URL "http://cms:8000/ingest.php"
+#define SIM_APN "your.apn"
+#define LORA_FREQUENCY 433E6
+#define LORA_NSS 5
+#define LORA_RST 14
+#define LORA_DIO0 26
+#define SD_CS 15
+#define SIM_RX_PIN 16
+#define SIM_TX_PIN 17
+#define SIM_PWRKEY_PIN 4
+#define UPLOADER_INTERVAL_MS 10000
+#define API_KEY_FALLBACK "changeme"
