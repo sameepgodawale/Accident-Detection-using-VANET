78c24ad0b15a0138f04eb4371d2096cd800c20a3
Sameep Godawale <57657376+sameepgodawale@users.noreply.github.com>
2025-09-30 08:28:36 +0530

    Update event.h

--- vanet-system/OBU/firmware/include/event.h
+++ vanet-system/OBU/firmware/include/event.h
@@ -1,17 +1,18 @@
 #pragma once
-#include <stdint.h>
-#define EVENT_UUID_LEN 37
-
-typedef enum { EVT_PENDING=0, EVT_SENT=1, EVT_ACKED=2, EVT_FAILED=3 } evt_status_t;
 
 typedef struct {
-    char uuid[EVENT_UUID_LEN];
-    uint32_t timestamp; // unix
-    float lat, lon;
-    uint8_t type; // 1=CRASH,2=FIRE
-    uint8_t severity; // 0..3
-    uint8_t retry_count;
-    evt_status_t status;
-} event_meta_t;
+    char device_id[16];
+    float acc_delta;
+    float gyro_delta;
+    float impact_time;
+    float lat;
+    float lon;
+    int has_gps;
+    int airbag_deployed;
+    float wheel_speed_before;
+    float wheel_speed_after;
+    float wheel_speed_drop_pct;
+} event_payload_t;
 
-#endif
+void event_manager_init(void);
+void event_manager_loop(void);
