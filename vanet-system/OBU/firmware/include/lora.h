2131180d4c8ab5f0f7bdd09aa9a0050e653a4d37
Sameep Godawale <57657376+sameepgodawale@users.noreply.github.com>
2025-09-30 08:29:28 +0530

    Update lora.h

--- vanet-system/OBU/firmware/include/lora.h
+++ vanet-system/OBU/firmware/include/lora.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <stdint.h>
-int lora_init(void);
-int lora_send(const uint8_t *buf, uint8_t len);
-int lora_receive(uint8_t *buf, uint8_t maxlen);
+
+void lora_init(void);
+int lora_send(const char *payload);
